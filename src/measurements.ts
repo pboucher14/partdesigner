fdf0c592976b019d75f3f021329ea7b69798ce83
Patrice Boucher <patrice_boucher@msn.com>
2021-02-24 14:21:28 -0500

    Added PBO Catalog and custom measurements

--- src/measurements.ts
+++ src/measurements.ts
@@ -6,12 +6,12 @@ class Measurements {
 	pinHoleRadius = 2.6 / this.technicUnit;
 	pinHoleOffset = 0.7 / this.technicUnit;
 	axleHoleSize = 1.01 / this.technicUnit;
-	pinRadius = 2.315 / this.technicUnit;
+	pinRadius = 2.73 / this.technicUnit;
 	ballBaseRadius = 1.6 / this.technicUnit;
 	ballRadius = 3.0 / this.technicUnit;
 	pinLipRadius = 0.17 / this.technicUnit;
-	axleSizeInner = 0.86 / this.technicUnit;
-	axleSizeOuter = 2.15 / this.technicUnit;
+	axleSizeInner = 0.87 / this.technicUnit;
+	axleSizeOuter = 2.11 / this.technicUnit;
 	attachmentAdapterSize = 0.4 / this.technicUnit;
 	attachmentAdapterRadius = 3 / this.technicUnit;
 	interiorEndMargin = 0.2 / this.technicUnit;
